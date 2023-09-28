626fa61227163a25ee6fc66b9127a913e9e59bef
0x0025 <116722706+0x002500@users.noreply.github.com>
2023-09-28 12:18:54 +0000

    [Build] Use cmake to control the project

---
+++ build/CMakeFiles/ddos.h.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ddos.h.dir/ddos.h.cpp.o"
+  "CMakeFiles/ddos.h.dir/ddos.h.cpp.o.d"
+  "libddos.h.a"
+  "libddos.h.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ddos.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/ddos.cpp
+++ src/ddos.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "httplib.h"
+#include "../include/httplib.h"
 
 using namespace std;
 using namespace httplib;
 
 int main(void)
 {
-    httplib::Client cli("https://cpp-httplib-server.yhirose.repl.co");
+    httplib::Client cli("http://cpp-httplib-server.yhirose.repl.co");
 
     auto res = cli.Get("/hi");
 }
